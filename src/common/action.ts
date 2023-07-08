e82c53d32a4d271351742926e360188d16f88f3d
Olga Chernega <eliang.cheng@gmail.com>
2023-07-08 16:45:24 +0300

    feat: add action and error types

---
+++ src/common/action.ts
@@ -0,0 +1,17 @@
+const enum Action {
+  PLAYER_AUTH = 'reg',
+  CREATE_GAME = 'create_game',
+  START_GAME = 'start_game',
+  PLAYER_TURN = 'turn',
+  ATTACK = 'attack',
+  FINISH = 'finish',
+  CREATE_ROOM = 'create_room',
+  UPDATE_ROOM = 'update_room',
+  UPDATE_WINNERS = 'update_winners',
+  ADD_PLAYER_TO_ROOM = 'add_user_to_room',
+  ADD_SHIPS = 'add_ships',
+  RANDOM_BATTLEFIELD = 'randomAttack',
+  ERROR = 'server_error',
+}
+
+export default Action;
