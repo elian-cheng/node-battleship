cafd06b1785a3ca2ca6ce3415674c725cae0e2b0
Olga Chernega <eliang.cheng@gmail.com>
2023-07-08 20:50:42 +0300

    feat: add ship model

--- src/interfaces/game.ts
+++ src/interfaces/game.ts
@@ -5,3 +5,20 @@ export interface IGameState {
     index: number;
   }[];
 }
+
+export interface IShip {
+  position: {
+    x: number;
+    y: number;
+  };
+  direction: boolean;
+  type: string;
+  length: number;
+}
+
+export const enum Attack {
+  MISSED = 'miss',
+  KILLED = 'killed',
+  SHOT = 'shot',
+  ERROR = 'error',
+}
